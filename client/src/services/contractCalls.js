78b48ac1c40c2ef2273677104ff351af05e425c2
Tarun Kumar Singh <tarunkumarsingh98@gmail.com>
2021-02-20 20:18:24 +0530

    contract functions

---
+++ client/src/services/contractCalls.js
@@ -0,0 +1,102 @@
+import compiled from "../contracts/Medical.json";
+
+const abi = compiled.abi;
+const bytecode = compiled.bytecode;
+
+export const deploy = async function () {
+  const web3 = window.web3;
+  const accountsAvailable = await web3.eth.getAccounts();
+  const instance = new web3.eth.Contract(abi);
+
+  instance
+    .deploy({
+      data: bytecode,
+    })
+    .send({
+      from: accountsAvailable[0],
+    })
+    .then((deployment) => {
+      console.log(
+        "Contract was deployed at the following address:",
+        deployment.options.address
+      );
+      return deployment.options.address;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+export const askReadPermission = async (account, address) => {
+  try {
+    const web3 = window.web3;
+    const contract = new web3.eth.Contract(abi, address);
+    const response = await contract.methods
+      .ReadPermission()
+      .send({ from: account });
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const askWritePermission = async (account, address) => {
+  try {
+    const web3 = window.web3;
+    const contract = new web3.eth.Contract(abi, address);
+    const response = await contract.methods
+      .WritePermission()
+      .send({ from: account });
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const handleRead = async (account, address) => {
+  try {
+    const web3 = window.web3;
+    const contract = new web3.eth.Contract(abi, address);
+    const response = await contract.methods.Read().send({ from: account });
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const handleWrite = async (account, address) => {
+  try {
+    const web3 = window.web3;
+    const contract = new web3.eth.Contract(abi, address);
+    const response = await contract.methods.Write().send({ from: account });
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const grantReadPermission = async (account, address) => {
+  try {
+    const web3 = window.web3;
+    const contract = new web3.eth.Contract(abi, address);
+    const response = await contract.methods
+      .GrantReadPermission()
+      .send({ from: account });
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const grantWritePermission = async (account, address) => {
+  try {
+    const web3 = window.web3;
+    const contract = new web3.eth.Contract(abi, address);
+    const response = await contract.methods
+      .GrantWritePermission()
+      .send({ from: account });
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+};
