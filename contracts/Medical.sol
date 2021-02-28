9142a45e1a166b8c08a469e2cf26162eccc03308
Tarun Kumar Singh <tarunkumarsingh98@gmail.com>
2021-02-28 22:51:42 +0530

    add Medical.sol

---
+++ contracts/Medical.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.5.0;
+
+contract Medical {
+    address private owner;
+    string private medicalLocationHash;
+
+    address public reader;
+    address public writer;
+    mapping(address => bool) canRead;
+    mapping(address => bool) canWrite;
+
+    constructor() public {
+        owner = tx.origin;
+    }
+
+    function Read() public returns (string memory) {
+        require(canRead[msg.sender] == true, "You do not have read permission");
+        return medicalLocationHash;
+    }
+
+    function Write(string memory locationHash) public {
+        require(
+            canWrite[msg.sender] == true,
+            "You do not have write permission"
+        );
+        medicalLocationHash = locationHash;
+        canWrite[msg.sender] = false;
+    }
+
+    function ReadPermission() public {
+        reader = msg.sender;
+    }
+
+    function WritePermission() public {
+        writer = msg.sender;
+    }
+
+    function GrantWritePermission() public {
+        require(owner == msg.sender, "You can not grant write permission");
+        require(writer != address(0), "No writer!");
+        canWrite[writer] = true;
+    }
+
+    function GrantReadPermission() public {
+        require(owner == msg.sender, "You can not grant read permission");
+        require(reader != address(0), "No reader!");
+        canRead[reader] = true;
+    }
+}
+
+// contract Adoption {
+//     address[16] public adopters;
+
+//     // Adopting a pet
+//     function adopt(uint256 petId) public returns (uint256) {
+//         require(petId >= 0 && petId <= 15);
+//         adopters[petId] = msg.sender;
+//         return petId;
+//     }
+
+//     // Retrieving the adopters
+//     function getAdopters() public view returns (address[16] memory) {
+//         return adopters;
+//     }
+// }
